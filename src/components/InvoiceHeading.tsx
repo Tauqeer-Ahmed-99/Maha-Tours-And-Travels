a68a92457348df0c664993a1b0dc893263453ac2
Tanvir04khan <tauqueerrkhan@gmail.com>
2024-04-04 00:09:35 +0530

    added: quick save note

--- src/components/InvoiceHeading.tsx
+++ src/components/InvoiceHeading.tsx
@@ -21,65 +21,70 @@ const InvoiceHeading = ({
   setInvoiceDate: (date: string) => void;
 }) => {
   return (
-    <Box
-      mb={2}
-      sx={{
-        display: { lg: "flex" },
-        justifyContent: { lg: "space-between" },
-        alignItems: { lg: "center" },
-      }}
-    >
-      <Typography level="title-lg">
-        {isCreatingNewInvoice
-          ? "New Invoice"
-          : invoice?.billToCustomer.name?.trim() &&
-            invoice?.billToCustomer.name?.trim().length > 0
-          ? `${invoice?.billToCustomer.name}'s Invoice`
-          : "Customer's Invoice"}
-      </Typography>
+    <>
       <Box
+        mb={2}
         sx={{
           display: { lg: "flex" },
           justifyContent: { lg: "space-between" },
           alignItems: { lg: "center" },
         }}
       >
-        <Box>
-          <FormControl>
-            {/* <FormLabel>Travellerage Type</FormLabel> */}
-            <RadioGroup
-              onChange={(e) =>
-                setTravellingType(e.target.value as TravellingType)
-              }
-              value={invoice?.travellingType ?? ""}
-            >
-              <Box display="flex" gap={2}>
-                <Radio value="Hajj" label="Hajj" variant="soft" />
-                <Radio value="Umrah" label="Umrah" variant="soft" />
-                <Radio value="Other" label="Others" variant="soft" />
-              </Box>
-            </RadioGroup>
-          </FormControl>
+        <Typography level="title-lg">
+          {isCreatingNewInvoice
+            ? "New Invoice"
+            : invoice?.billToCustomer.name?.trim() &&
+              invoice?.billToCustomer.name?.trim().length > 0
+            ? `${invoice?.billToCustomer.name}'s Invoice`
+            : "Customer's Invoice"}
+        </Typography>
+        <Box
+          sx={{
+            display: { lg: "flex" },
+            justifyContent: { lg: "space-between" },
+            alignItems: { lg: "center" },
+          }}
+        >
+          <Box>
+            <FormControl>
+              {/* <FormLabel>Travellerage Type</FormLabel> */}
+              <RadioGroup
+                onChange={(e) =>
+                  setTravellingType(e.target.value as TravellingType)
+                }
+                value={invoice?.travellingType ?? ""}
+              >
+                <Box display="flex" gap={2}>
+                  <Radio value="Hajj" label="Hajj" variant="soft" />
+                  <Radio value="Umrah" label="Umrah" variant="soft" />
+                  <Radio value="Other" label="Others" variant="soft" />
+                </Box>
+              </RadioGroup>
+            </FormControl>
+          </Box>
+          <Divider orientation="vertical" sx={{ mx: 2 }} />
+          <Typography level="title-md" sx={{ my: { xs: 2, lg: 0 } }}>
+            Invoice Number:{" "}
+            <b>{invoice?.travellingType + "-" + invoice?.invoiceNumber}</b>
+          </Typography>
+          <Divider orientation="vertical" sx={{ mx: 2 }} />
+          <Typography level="title-md" mr={1}>
+            Date:
+          </Typography>
+          <InvoiceInput
+            value={
+              invoice?.date.toISOString().split("T")[0] ??
+              new Date().toISOString().split("T")[0]
+            }
+            onChange={(e) => setInvoiceDate(e.target.value)}
+            type="date"
+          />
         </Box>
-        <Divider orientation="vertical" sx={{ mx: 2 }} />
-        <Typography level="title-md" sx={{ my: { xs: 2, lg: 0 } }}>
-          Invoice Number:{" "}
-          <b>{invoice?.travellingType + "-" + invoice?.invoiceNumber}</b>
-        </Typography>
-        <Divider orientation="vertical" sx={{ mx: 2 }} />
-        <Typography level="title-md" mr={1}>
-          Date:
-        </Typography>
-        <InvoiceInput
-          value={
-            invoice?.date.toISOString().split("T")[0] ??
-            new Date().toISOString().split("T")[0]
-          }
-          onChange={(e) => setInvoiceDate(e.target.value)}
-          type="date"
-        />
       </Box>
-    </Box>
+      <Typography level="body-sm" mb={2}>
+        Quickly save invoice with ctrl+s
+      </Typography>
+    </>
   );
 };
 
